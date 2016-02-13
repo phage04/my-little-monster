319eff1fa16381fa299dc5270bd0f04fa25064e3
lylejover <lylejover2yahoo.com>
2016-02-13 17:33:36 +0800

    Timer, Death, drag to golem

--- My Little Monster/DragImage.swift
+++ My Little Monster/DragImage.swift
@@ -34,6 +34,17 @@ class DragImg: UIImageView {
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        if let touch = touches.first, let target = dropTarget   {
+            
+            let position = touch.locationInView(self.superview)
+            
+            if CGRectContainsPoint(target.frame, position) {
+                
+                NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "onTargetDropped", object: nil))
+            }
+            
+        }
        self.center = originalPosition
     }
     
--- My Little Monster/ViewController.swift
+++ My Little Monster/ViewController.swift
@@ -12,17 +12,82 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var monsterImg: MonsterImg!
     @IBOutlet weak var foodImg: DragImg!
-    @IBOutlet weak var hearImg: DragImg!
+    @IBOutlet weak var heartImg: DragImg!
+    @IBOutlet weak var penalty1Img: UIImageView!
+    @IBOutlet weak var penalty2Img: UIImageView!
+    @IBOutlet weak var penalty3Img: UIImageView!
+    
+    let DIM_ALPHA: CGFloat = 0.2
+    let OPAQUE: CGFloat = 1.0
+    let MAX_PENALTIES = 3
+    
+    var penalties = 0
+    var timer: NSTimer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
         
+        foodImg.dropTarget = monsterImg
+        heartImg.dropTarget = monsterImg
+        
+        penalty1Img.alpha = DIM_ALPHA
+        penalty2Img.alpha = DIM_ALPHA
+        penalty3Img.alpha = DIM_ALPHA
+    
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter:", name: "onTargetDropped", object: nil)
 
+        startTimer()
+        
+    }
+    
+    func itemDroppedOnCharacter(notif: AnyObject) {
+        
+        print("ITEMDROPPED")
         
         
     }
     
+    func startTimer() {
+        
+        if timer != nil{
+            timer.invalidate()
+        }
+        
+        NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "changeGameState", userInfo: nil, repeats: true)
+        
+    }
+    
+    func changeGameState(){
+        
+        penalties++
+        
+        if penalties == 1 {
+            
+            penalty1Img.alpha = OPAQUE
+            penalty2Img.alpha = DIM_ALPHA
+            
+        } else if penalties == 2 {
+            penalty2Img.alpha = OPAQUE
+            penalty3Img.alpha = DIM_ALPHA
+        } else if penalties >= 3 {
+            penalty3Img.alpha = OPAQUE
+            
+        } else {
+            penalty1Img.alpha = DIM_ALPHA
+            penalty2Img.alpha = DIM_ALPHA
+            penalty3Img.alpha = DIM_ALPHA
+        }
+        
+        if penalties >= MAX_PENALTIES {
+            gameOver()
+        }
+    }
+    
+    func gameOver(){
+
+        timer.invalidate()
+        monsterImg.playDeathAnimation()
+    }
 
 
 
